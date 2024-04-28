f04a48944133fa84ef10497f571cc38c94f4c821
YunaKiri <168326265+YunaKiri@users.noreply.github.com>
2024-04-28 12:06:29 -0300

    Update toast.js

--- toast.js
+++ toast.js
@@ -2,6 +2,6 @@ Java.perform(function () {
 
     Java.scheduleOnMainThread(function() {
             var toast = Java.use("android.widget.Toast");
-            toast.makeText(Java.use("android.app.ActivityThread").currentApplication().getApplicationContext(), Java.use("java.lang.String").$new("GABRIEL ESTÁ FUNCIONANDO✓✓✓"), 1).show();
+            toast.makeText(Java.use("android.app.ActivityThread").currentApplication().getApplicationContext(), Java.use("java.lang.String").$new("GABRIEL ESTÁ FUNCIONANDO✓✓"), 1).show();
     });
 }); 
