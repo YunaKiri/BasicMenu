1f2dd9f140c497ba90933b81e4c98f5eff30e29a
YunaKiri <168326265+YunaKiri@users.noreply.github.com>
2024-04-28 13:00:23 -0300

    Delete toast.js

--- toast.js
+++
@@ -1,7 +0,0 @@
-Java.perform(function () { 
-
-    Java.scheduleOnMainThread(function() {
-            var toast = Java.use("android.widget.Toast");
-            toast.makeText(Java.use("android.app.ActivityThread").currentApplication().getApplicationContext(), Java.use("java.lang.String").$new("GABRIEL ESTÁ FUNCIONANDO✓✓"), 1).show();
-    });
-}); 
