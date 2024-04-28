88b64cd620625784bfe6845e77b896401badc9c6
YunaKiri <168326265+YunaKiri@users.noreply.github.com>
2024-04-28 12:05:35 -0300

    Create toast.js

---
+++ toast.js
@@ -0,0 +1,7 @@
+Java.perform(function () { 
+
+    Java.scheduleOnMainThread(function() {
+            var toast = Java.use("android.widget.Toast");
+            toast.makeText(Java.use("android.app.ActivityThread").currentApplication().getApplicationContext(), Java.use("java.lang.String").$new("GABRIEL ESTÁ FUNCIONANDO✓✓✓"), 1).show();
+    });
+}); 
