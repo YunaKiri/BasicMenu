d8197c7322c633450b5b35d889228784fb7c2074
Tick01 <168366996+Tick01@users.noreply.github.com>
2024-05-23 17:53:05 -0300

    Update 64wz.js

--- 64wz.js
+++ 64wz.js
@@ -767,28 +767,290 @@ Java.perform(function () {
         
         menu.addOption("option1", "TP X", {
             on: function () {
-                // Add actions when the option is turned on
+                let colisionEnable = Module.getBaseAddress("libil2cpp.so").add(0x264EE8C)
+let fcolisionEnable = new NativeFunction(colisionEnable,"void",["pointer","bool"])
+
+var posx 
+var posy
+var posz
+
+let vect 
+
+let compponent =  Module.getBaseAddress("libil2cpp.so").add(0x25E4AE4)
+let fcompponent = new NativeFunction(compponent,"pointer",["pointer"]) 
+
+
+let pushLocalPosition = Module.getBaseAddress("libil2cpp.so").add(0x25F2B38)
+let fPushLocalPostion = new NativeFunction(pushLocalPosition,"void",["pointer","pointer"])
+
+
+let setPosition = Module.getBaseAddress("libil2cpp.so").add(0x25F2CB4)
+let fsetPosition = new NativeFunction(setPosition,"void",["pointer","float","float","float"])
+
+const base = Module.getBaseAddress("libil2cpp.so").add(0xD4B2D8)
+
+Interceptor.attach(base,{
+    onEnter(args){
+     vect = Memory.alloc(3*4)
+   
+     let transform = fcompponent(args[0])
+     
+     fPushLocalPostion(transform,vect)
+
+    posx = vect.readFloat()+0.5
+    posy = vect.add(0x4).readFloat()
+    posz = vect.add(0x8).readFloat()
+
+     fsetPosition(transform,posx,posy,posz)
+
+    }})
+
             },
             off: function () {
-                // Add actions when the option is turned off
+              let colisionEnable = Module.getBaseAddress("libil2cpp.so").add(0x264EE8C)
+let fcolisionEnable = new NativeFunction(colisionEnable,"void",["pointer","bool"])
+
+var posx 
+var posy
+var posz
+
+let vect 
+
+let compponent =  Module.getBaseAddress("libil2cpp.so").add(0x25E4AE4)
+let fcompponent = new NativeFunction(compponent,"pointer",["pointer"]) 
+
+
+let pushLocalPosition = Module.getBaseAddress("libil2cpp.so").add(0x25F2B38)
+let fPushLocalPostion = new NativeFunction(pushLocalPosition,"void",["pointer","pointer"])
+
+
+let setPosition = Module.getBaseAddress("libil2cpp.so").add(0x25F2CB4)
+let fsetPosition = new NativeFunction(setPosition,"void",["pointer","float","float","float"])
+
+const base = Module.getBaseAddress("libil2cpp.so").add(0xD4B2D8)
+
+Interceptor.attach(base,{
+    onEnter(args){
+     vect = Memory.alloc(3*4)
+   
+     let transform = fcompponent(args[0])
+     
+     fPushLocalPostion(transform,vect)
+
+    posx = vect.readFloat()
+    posy = vect.add(0x4).readFloat()
+    posz = vect.add(0x8).readFloat()
+
+     fsetPosition(transform,posx,posy,posz)
+
+    }})
+
             }
         })
         
                 menu.addOption("option2", "TP Y", {
             on: function () {
-                // Add actions when the option is turned on
+      let colisionEnable = Module.getBaseAddress("libil2cpp.so").add(0x264EE8C)
+let fcolisionEnable = new NativeFunction(colisionEnable,"void",["pointer","bool"])
+
+var posx 
+var posy
+var posz
+
+let vect 
+
+let compponent =  Module.getBaseAddress("libil2cpp.so").add(0x25E4AE4)
+let fcompponent = new NativeFunction(compponent,"pointer",["pointer"]) 
+
+
+let pushLocalPosition = Module.getBaseAddress("libil2cpp.so").add(0x25F2B38)
+let fPushLocalPostion = new NativeFunction(pushLocalPosition,"void",["pointer","pointer"])
+
+
+let setPosition = Module.getBaseAddress("libil2cpp.so").add(0x25F2CB4)
+let fsetPosition = new NativeFunction(setPosition,"void",["pointer","float","float","float"])
+
+const base = Module.getBaseAddress("libil2cpp.so").add(0xD4B2D8)
+
+Interceptor.attach(base,{
+    onEnter(args){
+     vect = Memory.alloc(3*4)
+   
+     let transform = fcompponent(args[0])
+     
+     fPushLocalPostion(transform,vect)
+
+    posx = vect.readFloat()
+    posy = vect.add(0x4).readFloat()+2
+    posz = vect.add(0x8).readFloat()
+
+     fsetPosition(transform,posx,posy,posz)
+
+    }})
+
             },
             off: function () {
-                // Add actions when the option is turned off
+        let colisionEnable = Module.getBaseAddress("libil2cpp.so").add(0x264EE8C)
+let fcolisionEnable = new NativeFunction(colisionEnable,"void",["pointer","bool"])
+
+var posx 
+var posy
+var posz
+
+let vect 
+
+let compponent =  Module.getBaseAddress("libil2cpp.so").add(0x25E4AE4)
+let fcompponent = new NativeFunction(compponent,"pointer",["pointer"]) 
+
+
+let pushLocalPosition = Module.getBaseAddress("libil2cpp.so").add(0x25F2B38)
+let fPushLocalPostion = new NativeFunction(pushLocalPosition,"void",["pointer","pointer"])
+
+
+let setPosition = Module.getBaseAddress("libil2cpp.so").add(0x25F2CB4)
+let fsetPosition = new NativeFunction(setPosition,"void",["pointer","float","float","float"])
+
+const base = Module.getBaseAddress("libil2cpp.so").add(0xD4B2D8)
+
+Interceptor.attach(base,{
+    onEnter(args){
+     vect = Memory.alloc(3*4)
+   
+     let transform = fcompponent(args[0])
+     
+     fPushLocalPostion(transform,vect)
+
+    posx = vect.readFloat()
+    posy = vect.add(0x4).readFloat()
+    posz = vect.add(0x8).readFloat()
+
+     fsetPosition(transform,posx,posy,posz)
+
+    }})
+
             }
         })
         
                 menu.addOption("option3", "TP Z", {
             on: function () {
-                // Add actions when the option is turned on
+             let colisionEnable = Module.getBaseAddress("libil2cpp.so").add(0x264EE8C)
+let fcolisionEnable = new NativeFunction(colisionEnable,"void",["pointer","bool"])
+
+var posx 
+var posy
+var posz
+
+let vect 
+
+let compponent =  Module.getBaseAddress("libil2cpp.so").add(0x25E4AE4)
+let fcompponent = new NativeFunction(compponent,"pointer",["pointer"]) 
+
+
+let pushLocalPosition = Module.getBaseAddress("libil2cpp.so").add(0x25F2B38)
+let fPushLocalPostion = new NativeFunction(pushLocalPosition,"void",["pointer","pointer"])
+
+
+let setPosition = Module.getBaseAddress("libil2cpp.so").add(0x25F2CB4)
+let fsetPosition = new NativeFunction(setPosition,"void",["pointer","float","float","float"])
+
+const base = Module.getBaseAddress("libil2cpp.so").add(0xD4B2D8)
+
+Interceptor.attach(base,{
+    onEnter(args){
+     vect = Memory.alloc(3*4)
+   
+     let transform = fcompponent(args[0])
+     
+     fPushLocalPostion(transform,vect)
+
+    posx = vect.readFloat()
+    posy = vect.add(0x4).readFloat()
+    posz = vect.add(0x8).readFloat()+0.5
+
+     fsetPosition(transform,posx,posy,posz)
+
+
+    }})
             },
             off: function () {
-                // Add actions when the option is turned off
+               let colisionEnable = Module.getBaseAddress("libil2cpp.so").add(0x264EE8C)
+let fcolisionEnable = new NativeFunction(colisionEnable,"void",["pointer","bool"])
+
+var posx 
+var posy
+var posz
+
+let vect 
+
+let compponent =  Module.getBaseAddress("libil2cpp.so").add(0x25E4AE4)
+let fcompponent = new NativeFunction(compponent,"pointer",["pointer"]) 
+
+
+let pushLocalPosition = Module.getBaseAddress("libil2cpp.so").add(0x25F2B38)
+let fPushLocalPostion = new NativeFunction(pushLocalPosition,"void",["pointer","pointer"])
+
+
+let setPosition = Module.getBaseAddress("libil2cpp.so").add(0x25F2CB4)
+let fsetPosition = new NativeFunction(setPosition,"void",["pointer","float","float","float"])
+
+const base = Module.getBaseAddress("libil2cpp.so").add(0xD4B2D8)
+
+Interceptor.attach(base,{
+    onEnter(args){
+     vect = Memory.alloc(3*4)
+   
+     let transform = fcompponent(args[0])
+     
+     fPushLocalPostion(transform,vect)
+
+    posx = vect.readFloat()
+    posy = vect.add(0x4).readFloat()
+    posz = vect.add(0x8).readFloat()
+
+     fsetPosition(transform,posx,posy,posz)
+
+    }})
+
+            }
+        })
+      menu.addOption("option4", "NoRecoil", {
+            on: function () {
+
+Interceptor.attach(base,{
+    onEnter(args){
+ 
+const arma = args[0].add(0x110).readPointer().add(0x140).readPointer()
+
+arma.add(0x1C4).writeFloat(0)
+
+arma.add(0x1C8).writeFloat(0)
+
+arma.add(0x1DC).writeFloat(0)
+
+arma.add(0x1E0).writeFloat(0)
+
+arma.add(0x1C4).writeFloat(0)
+
+    }})
+            },
+            off: function () {
+  
+Interceptor.attach(base,{
+    onEnter(args){
+   
+const arma = args[0].add(0x110).readPointer().add(0x140).readPointer()
+
+arma.add(0x1C4).writeFloat(0)
+
+arma.add(0x1C8).writeFloat(0)
+
+arma.add(0x1DC).writeFloat(0)
+
+arma.add(0x1E0).writeFloat(0)
+
+arma.add(0x1C4).writeFloat(0)
+    }})
+
             }
         })
 
